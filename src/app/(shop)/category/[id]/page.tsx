0c89cf4ca2c89ecddb33f9e29d4ef14ae73768ae
alex-arraga <alex-arraga@users.noreply.github.com>
2024-07-15 22:37:10 -0300

    Category: Not found page

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,5 +1,17 @@
+import { notFound } from "next/navigation";
 
-export default function() {
+interface Props {
+  params: {
+    id: string
+  }
+}
+
+export default function ({ params }: Props) {
+  
+  if(params.id === 'kids') {
+    notFound()
+  }
+
   return (
     <div>
       <h1>Category page</h1>
