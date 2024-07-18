15997698a947d1f738e785a2964512eb7be151aa
alex-arraga <alex-arraga@users.noreply.github.com>
2024-07-17 22:00:55 -0300

    Product page: Desktop slideshow

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { initialData } from "@/seed/seed";
 import notFound from "../not-found";
 import { titleFont } from "@/config/fonts";
-import { QuantitySelector, SizeSelector } from "@/components";
+import { ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
 
 interface Props {
   params: {
@@ -20,15 +20,18 @@ export default function ProductPage({ params }: Props) {
 
 
   return (
-    <section className="grid grid-cols-1 md:grid-cols-3 gap-2 w-full">
+    <section className="grid grid-cols-1 md:grid-cols-4 gap-2 w-full">
 
       {/* Slideshow */}
-      <div className="col-span-1 md:col-span-2 bg-gray-300">
-        Slideshow
+      <div className="col-span-2 rounded-xl p-4">
+        <ProductSlideshow
+          images={product?.images!}
+          title={product?.title!}
+        />
       </div>
 
       {/* Product details */}
-      <div className="md:col-span-1 p-4 rounded">
+      <div className="md:col-span-2 p-4 rounded">
         <div className="flex flex-col mb-6">
           <h1 className={`${titleFont.className} text-2xl font-bold`}>{product?.title}</h1>
           <span className="font-semibold text-lg">${product?.price}</span>
@@ -61,3 +64,50 @@ export default function ProductPage({ params }: Props) {
     </section>
   );
 }
+
+
+
+// return (
+//   <section className="grid grid-cols-1 md:grid-cols-3 gap-2 w-full">
+
+//     {/* Slideshow */}
+//     <div className="col-span-1 md:col-span-2 bg-gray-300 rounded-xl p-6">
+//       <ProductSlideshow
+//         images={product?.images!}
+//         title={product?.title!}
+//       />
+//     </div>
+
+//     {/* Product details */}
+//     <div className="md:col-span-1 p-4 rounded">
+//       <div className="flex flex-col mb-6">
+//         <h1 className={`${titleFont.className} text-2xl font-bold`}>{product?.title}</h1>
+//         <span className="font-semibold text-lg">${product?.price}</span>
+//       </div>
+
+
+//       {/* Selector de Tallas */}
+//       <SizeSelector
+//         availableSizes={product?.sizes!}
+//         selectedSize={product?.sizes[0]!}
+//       />
+
+
+//       {/* Selector de cantidad */}
+//       <QuantitySelector
+//         quantity={3}
+//       />
+
+//       {/* Button */}
+//       <button className="btn-primary mb-6">
+//         Agregar al carrito
+//       </button>
+
+
+
+//       {/* Descripción */}
+//       <h3 className="font-bold my-6">Descripción</h3>
+//       <p>{product?.description}</p>
+//     </div>
+//   </section>
+// );
---
+++ src/components/product/slideshow/ProductSlideshow.tsx
@@ -0,0 +1,88 @@
+'use client'
+
+import React, { useRef, useState } from 'react';
+import Image from 'next/image';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+
+import './slideshow.css';
+
+
+// Import Swiper React components
+import { Swiper as SwiperObject } from 'swiper';
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Autoplay, FreeMode, Navigation, Thumbs } from 'swiper/modules';
+
+
+interface Props {
+  images: string[];
+  title: string;
+  className?: string;
+}
+
+
+export const ProductSlideshow = ({ images, title, className }: Props) => {
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperObject>();
+
+  return (
+    <div className={className}>
+      <Swiper
+        style={{
+          '--swiper-navigation-color': '#fff',
+          '--swiper-pagination-color': '#fff',
+        } as React.CSSProperties}
+        spaceBetween={10}
+        navigation={true}
+        autoplay={{ delay: 4000 }}
+        thumbs={{
+          swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null
+        }}
+        modules={[FreeMode, Navigation, Thumbs, Autoplay]}
+        className="mySwiper2"
+      >
+        {
+          images.map((image) => (
+            <SwiperSlide key={image}>
+              <Image
+                src={`/products/${image}`}
+                alt={title}
+                width={1024}
+                height={800}
+                className='rounded-xl object-fill'
+              />
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
+
+      {/* Parte 2 */}
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper"
+      >
+        {
+          images.map((image) => (
+            <SwiperSlide key={image}>
+              <Image
+                src={`/products/${image}`}
+                alt={title}
+                width={300}
+                height={300}
+                className='rounded-xl object-fill'
+              />
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
+    </div>
+  );
+}
