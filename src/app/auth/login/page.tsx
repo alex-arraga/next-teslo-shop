97a04fc981d8e64fcfaebb71bd9bddbbf4ee81f7
alex-arraga <alex-arraga@users.noreply.github.com>
2024-07-22 17:55:29 -0300

    Login and Register pages Design

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,9 +1,51 @@
-import { titleFont } from "@/config/fonts";
+import Link from 'next/link';
+import { Title } from '@/components';
 
-export default function() {
+
+export default function LoginPage() {
   return (
-    <div>
-      <h1 className={`${titleFont.className} font-bold`}>Login</h1>
+    <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
+      <Title
+        title='Ingresar'
+      />
+
+      <div className="flex flex-col">
+
+        <label htmlFor="email">Correo electrónico</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="email"
+          placeholder='example@gmail.com'
+        />
+
+
+        <label htmlFor="email">Contraseña</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="password"
+          placeholder='******'
+        />
+
+        <button
+          className="btn-primary">
+          Ingresar
+        </button>
+
+
+        {/* divisor line */}
+        <div className="flex items-center my-5">
+          <div className="flex-1 border-t border-gray-500"></div>
+          <div className="px-2 text-gray-800">O</div>
+          <div className="flex-1 border-t border-gray-500"></div>
+        </div>
+
+        <Link
+          href="/auth/new-account"
+          className="btn-secondary text-center">
+          Crear una nueva cuenta
+        </Link>
+
+      </div>
     </div>
   );
 }
--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -1,9 +1,59 @@
-import { titleFont } from "@/config/fonts";
+import Link from 'next/link';
+import { Title } from '@/components';
 
-export default function() {
+
+export default function NewAccountPage() {
   return (
-    <div>
-      <h1 className={`${titleFont.className} font-bold`}>New account</h1>
+    <div className="flex flex-col min-h-screen pt-32 sm:pt-52">
+      <Title
+        title='Registrarse'
+      />
+
+      <div className="flex flex-col">
+
+        <label htmlFor="email">Nombre completo</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="text"
+          placeholder='John Doe'
+        />
+
+
+        <label htmlFor="email">Correo electrónico</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="email"
+          placeholder='example@gmail.com'
+        />
+
+
+        <label htmlFor="email">Contraseña</label>
+        <input
+          className="px-5 py-2 border bg-gray-200 rounded mb-5"
+          type="password"
+          placeholder='******'
+        />
+
+        <button
+          className="btn-primary">
+          Crear cuenta
+        </button>
+
+
+        {/* divisor line */}
+        <div className="flex items-center my-5">
+          <div className="flex-1 border-t border-gray-500"></div>
+          <div className="px-2 text-gray-800">O</div>
+          <div className="flex-1 border-t border-gray-500"></div>
+        </div>
+
+        <Link
+          href="/auth/login"
+          className="btn-secondary text-center">
+          Ingresar
+        </Link>
+
+      </div>
     </div>
   );
 }
