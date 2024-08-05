2b94d5128dac8a7a0967635dfb81dad4b78850f9
alex-arraga <alex-arraga@users.noreply.github.com>
2024-08-05 19:06:42 -0300

    Cart: Show products from store

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -2,21 +2,12 @@ import Link from "next/link";
 // import { redirect } from "next/navigation";
 
 import { ProductsInCart, Title } from "@/components";
-import { initialData } from "@/seed/seed";
-
 import { IoChevronForward } from "react-icons/io5";
-
-// todo: replace for products in cookies
-const productsInCart = [
-  initialData.products[0],
-  initialData.products[1],
-  initialData.products[2],
-]
 
 
 export const metadata = {
- title: 'Cart',
- description: 'Cart',
+  title: 'Cart',
+  description: 'Cart',
 };
 
 
@@ -24,58 +15,54 @@ export default function CartPage() {
   // todo: redirect to empty if the user hasn't any product
   // redirect('/empty')
 
+
   return (
     <section className="flex justify-center w-full min-h-screen">
 
-        <div className="grid grid-cols-1 xl:grid-cols-2 xl:p-6 gap-6 xl:max-w-[calc(60vw)] h-fit">
-          <div className="flex flex-col col-span-2 xl:col-span-1 rounded-md">
-            <Title
-              title="Carrito de compra"
-            />
+      <div className="grid grid-cols-1 xl:grid-cols-2 xl:p-6 gap-6 xl:max-w-[calc(60vw)] h-fit">
 
-            <Link href='/cart' className="flex items-center gap-1 rounded-md mb-2 w-fit transform hover:translate-x-1 hover:text-blue-400 hover:font-semibold transition-all">
-              <IoChevronForward size={15} />
-              Seguir comprando
-            </Link>
+        <div className="flex flex-col col-span-2 xl:col-span-1 rounded-md">
+          <Title
+            title="Carrito de compra"
+          />
 
-            {/* Products in cart */}
-            {productsInCart.map((product) => (
-              <ProductsInCart
-                key={product.slug}
-                product={product}
-              />
-            ))}
-          </div>
+          <Link href='/cart' className="flex items-center gap-1 rounded-md mb-2 w-fit transform hover:translate-x-1 hover:text-blue-400 hover:font-semibold transition-all">
+            <IoChevronForward size={15} />
+            Seguir comprando
+          </Link>
 
-          {/* Checkout */}
-          <div className="mb-28 xl:mb-0 col-span-2 xl:col-span-1 bg-white rounded-md shadow-xl h-fit p-4 xl:p-6">
-            <Title
-              title="Resumen"
-            />
-
-            <div className="grid grid-cols-2">
-              <span>N° Productos</span>
-              <span className="text-right">3 articulos</span>
-
-              <span>Subtotal</span>
-              <span className="text-right">$100</span>
-
-              <span>Impuestos (15%)</span>
-              <span className="text-right">$15</span>
-
-              <span className="mt-5 text-lg font-bold">Total</span>
-              <span className="mt-5 text-lg font-bold text-right">$115</span>
-
-            </div>
-
-            <button className="w-full mt-4 btn-primary">
-              <Link href='/checkout/address' className="text-center block">
-                Continuar
-              </Link>
-            </button>
-          </div>
-
+          <ProductsInCart />
         </div>
+
+        {/* Checkout */}
+        <div className="mb-28 xl:mb-0 col-span-2 xl:col-span-1 bg-white rounded-md shadow-xl h-fit p-4 xl:p-6">
+          <Title
+            title="Resumen"
+          />
+
+          <div className="grid grid-cols-2">
+            <span>N° Productos</span>
+            <span className="text-right">3 articulos</span>
+
+            <span>Subtotal</span>
+            <span className="text-right">$100</span>
+
+            <span>Impuestos (15%)</span>
+            <span className="text-right">$15</span>
+
+            <span className="mt-5 text-lg font-bold">Total</span>
+            <span className="mt-5 text-lg font-bold text-right">$115</span>
+
+          </div>
+
+          <button className="w-full mt-4 btn-primary">
+            <Link href='/checkout/address' className="text-center block">
+              Continuar
+            </Link>
+          </button>
+        </div>
+
+      </div>
 
     </section>
   );
--- src/components/cart/products-in-cart/ProductsInCart.tsx
+++ src/components/cart/products-in-cart/ProductsInCart.tsx
@@ -1,44 +1,73 @@
+'use client'
+
+import { useEffect, useState } from "react";
+
 import Image from "next/image"
-import { Product } from "@/interfaces";
 import { QuantitySelector } from '@/components';
 
 import { IoTrashOutline } from "react-icons/io5";
+import { useCartStore } from "@/store";
+import Link from "next/link";
 
-interface Props {
-  product: Product
-}
 
-export const ProductsInCart = ({ product }: Props) => {
+export const ProductsInCart = () => {
+  const [loading, setloading] = useState(false)
+  const productsInCart = useCartStore(store => store.cart)
+
+  useEffect(() => {
+    setloading(true)
+  }, [])
+
+
+  if (!loading) {
+    return <p>Loading...</p>
+  }
+
   return (
-    <div className="flex p-4 bg-gray-50 rounded-lg my-2">
-      <Image
-        src={`/products/${product.images[0]}`}
-        alt={product.title}
-        width={120}
-        height={120}
-        style={{
-          width: '100px',
-          height: '100px'
-        }}
-        className="rounded object-contain mr-4"
-      />
+    <div className="w-full">
+      {
+        productsInCart.map((product) => (
+          <div
+            key={`${product.slug}-${product.size}`}
+            className="flex p-4 bg-gray-50 rounded-lg my-2"
+          >
+            <Image
+              src={`/products/${product.images}`}
+              alt={product.title}
+              width={120}
+              height={120}
+              style={{
+                width: '100px',
+                height: '100px'
+              }}
+              className="rounded object-contain mr-4"
+            />
 
-      <div className="flex flex-col w-full">
-        <div className="flex flex-col md:flex-row justify-between">
-          <h2 className="font-bold text-sm md:text-lg overflow-auto max-w-sm md:mr-10 w-full">{product.title}</h2>
-          <p className="text-sm md:text-base font-semibold">${product.price.toLocaleString()}</p>
-        </div>
+            <div className="flex flex-col w-full">
+              <Link
+                href={`/product/${product.slug}`}
+                className="flex flex-col md:flex-row justify-between"
+              >
+                <h2 className="hover:text-blue-700 transition-all font-bold text-sm md:text-lg overflow-auto max-w-sm md:mr-10 w-full">
+                  {product.size} - {product.title}
+                </h2>
+                <p className="text-sm md:text-base font-semibold">${product.price.toLocaleString()}</p>
+              </Link>
 
-        <QuantitySelector
-          quantity={3}
-          className="mb-4 text-sm md:text-base"
-        />
+              <QuantitySelector
+                quantity={3}
+                className="mb-4 text-sm md:text-base"
+                onQuantityChanged={value => console.log(value)}
+              />
 
-        <button className="flex items-center text-sm md:text-base gap-1 text-gray-500 hover:text-red-400 w-fit hover:font-semibold transition-all">
-          <IoTrashOutline size={20} />
-          Remover
-        </button>
-      </div>
+              <button className="flex items-center text-sm md:text-base gap-1 text-gray-500 hover:text-red-400 w-fit hover:font-semibold transition-all">
+                <IoTrashOutline size={20} />
+                Remover
+              </button>
+            </div>
+          </div>
+        ))
+      }
     </div>
   )
 }
