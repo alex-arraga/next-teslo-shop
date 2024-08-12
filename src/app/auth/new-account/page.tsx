b057a2f5bd45f753af58a24a078adb9c109ea77d
alex-arraga <alex-arraga@users.noreply.github.com>
2024-08-12 18:32:16 -0300

    Auth: Register Form with React Hook Form

--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link';
 import { Title } from '@/components';
+import { RegisterForm } from './ui/RegisterForm';
 
 
 export default function NewAccountPage() {
@@ -9,51 +9,7 @@ export default function NewAccountPage() {
         title='Registrarse'
       />
 
-      <div className="flex flex-col">
-
-        <label htmlFor="email">Nombre completo</label>
-        <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
-          type="text"
-          placeholder='John Doe'
-        />
-
-
-        <label htmlFor="email">Correo electrónico</label>
-        <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
-          type="email"
-          placeholder='example@gmail.com'
-        />
-
-
-        <label htmlFor="email">Contraseña</label>
-        <input
-          className="px-5 py-2 border bg-gray-200 rounded mb-5"
-          type="password"
-          placeholder='******'
-        />
-
-        <button
-          className="btn-primary">
-          Crear cuenta
-        </button>
-
-
-        {/* divisor line */}
-        <div className="flex items-center my-5">
-          <div className="flex-1 border-t border-gray-500"></div>
-          <div className="px-2 text-gray-800">O</div>
-          <div className="flex-1 border-t border-gray-500"></div>
-        </div>
-
-        <Link
-          href="/auth/login"
-          className="btn-secondary text-center">
-          Ingresar
-        </Link>
-
-      </div>
+     <RegisterForm />
     </div>
   );
 }
---
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -0,0 +1,88 @@
+'use client'
+
+import Link from "next/link"
+import { SubmitHandler, useForm } from "react-hook-form"
+
+type FormInputs = {
+  name: string
+  email: string
+  password: string
+}
+
+
+export const RegisterForm = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm<FormInputs>();
+
+  const onSumbit: SubmitHandler<FormInputs> = async (data) => {
+    const { email, name, password } = data;
+    console.log({ email, name, password })
+
+    // Server action
+  }
+
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSumbit)}
+      className="flex flex-col"
+    >
+
+      <label htmlFor="email">Nombre completo</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="text"
+        placeholder='John Doe'
+        autoFocus
+        {...register("name", { required: true })}
+      />
+
+
+      <label htmlFor="email">Correo electrónico</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="email"
+        placeholder='example@gmail.com'
+        autoFocus
+        {...register("email", { required: true })}
+      />
+
+
+      <label htmlFor="email">Contraseña</label>
+      <input
+        className="px-5 py-2 border bg-gray-200 rounded mb-5"
+        type="password"
+        placeholder='******'
+        autoFocus
+        {...register("password", { required: true })}
+      />
+
+
+      {
+        errors.name?.type === 'required' && (
+          <span className="text-sm text-red-600 bg-red-50 rounded p-1 mb-4">* El nombre es requerido</span>
+        )
+      }
+
+      <button
+        type="submit"
+        className="btn-primary">
+        Crear cuenta
+      </button>
+
+
+      {/* divisor line */}
+      <div className="flex items-center my-5">
+        <div className="flex-1 border-t border-gray-500"></div>
+        <div className="px-2 text-gray-800">O</div>
+        <div className="flex-1 border-t border-gray-500"></div>
+      </div>
+
+      <Link
+        href="/auth/login"
+        className="btn-secondary text-center">
+        Ingresar
+      </Link>
+
+    </form>
+  )
+}
