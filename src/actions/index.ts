0a3095af78ed52faff56ebad616d2da8c2e0e1ab
alex-arraga <alex-arraga@users.noreply.github.com>
2024-08-13 03:41:46 -0300

    wip

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,10 +1,10 @@
 // Products
-export * from './products/products-pagination';
+export { getPaginatedProductWithImages } from './products/products-pagination';
 export { getProductBySlug } from './products/get-product-by-slug';
 export { getStockBySlug } from './products/get-stock-by-slug';
 
 // Auth
-export {authenticate, login} from './auth/login'
+export { authenticate, login } from './auth/login'
 export { logout } from './auth/logout'
 export { registerUser } from './auth/register'
 
