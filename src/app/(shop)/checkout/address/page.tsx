275cf1cc06dda90c196696041d2ea1717cc17b2c
alex-arraga <alex-arraga@users.noreply.github.com>
2024-08-15 14:43:02 -0300

    Shipping: Address Form

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
 import { Title } from "@/components";
+import { AdressForm } from "./ui/AdressForm";
 
 
 export const metadata = {
- title: 'Checkout Address',
- description: 'Checkout Address',
+  title: 'Checkout Address',
+  description: 'Checkout Address',
 };
 
 
@@ -19,87 +19,7 @@ export default function CheckoutAddressPage() {
         />
 
         {/* Form */}
-        <form className="grid grid-cols-1 gap-2 sm:gap-5 sm:grid-cols-2">
-
-          <div className="flex flex-col mb-2">
-            <label>Nombres</label>
-            <input
-              type="text"
-              className="p-2 border rounded-md bg-gray-200"
-            />
-          </div>
-
-          <div className="flex flex-col mb-2">
-            <label>Apellidos</label>
-            <input
-              type="text"
-              className="p-2 border rounded-md bg-gray-200"
-            />
-          </div>
-
-          <div className="flex flex-col mb-2">
-            <label>Dirección</label>
-            <input
-              type="text"
-              className="p-2 border rounded-md bg-gray-200"
-            />
-          </div>
-
-          <div className="flex flex-col mb-2">
-            <label>Dirección 2 (opcional)</label>
-            <input
-              type="text"
-              className="p-2 border rounded-md bg-gray-200"
-            />
-          </div>
-
-          <div className="flex flex-col mb-2">
-            <label>Código postal</label>
-            <input
-              type="text"
-              className="p-2 border rounded-md bg-gray-200"
-            />
-          </div>
-
-          <div className="flex flex-col mb-2">
-            <label>Ciudad</label>
-            <input
-              type="text"
-              className="p-2 border rounded-md bg-gray-200"
-            />
-          </div>
-
-          <div className="flex flex-col mb-2">
-            <label>País</label>
-            <select
-              className="p-2 border rounded-md bg-gray-200 cursor-pointer"
-            >
-              <option value="">[ Seleccione ]</option>
-              <option value="ARG">Argentina</option>
-              <option value="BRZ">Brasil</option>
-              <option value="CRI">Costa Rica</option>
-            </select>
-          </div>
-
-          <div className="flex flex-col mb-2">
-            <label>Teléfono</label>
-            <input
-              type="text"
-              className="p-2 border rounded-md bg-gray-200"
-            />
-          </div>
-
-
-          {/* Button checkout */}
-          <div className="flex flex-col mt-4 sm:mt-10">
-            <Link
-              href='/checkout'
-              className="btn-primary flex w-full sm:w-1/2 justify-center ">
-              Siguiente
-            </Link>
-          </div>
-          
-        </form>
+        <AdressForm />
 
       </div>
     </div>
