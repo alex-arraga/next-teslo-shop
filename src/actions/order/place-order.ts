3a401c4fffdbb1b98eff07a5f2eadb704dc8b315
alex-arraga <alex-arraga@users.noreply.github.com>
2024-08-19 20:08:47 -0300

    Amounts calculate

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import prisma from "@/lib/prisma"
 import { auth } from "@/auth.config"
 import type { Address, Size } from "@/interfaces"
 
@@ -22,7 +23,44 @@ export const placeOrder = async (productsInOrder: ProductToOrder[], address: Add
       }
     }
 
-    console.log({ products: productsInOrder, address, userId })
+    // Obtener info de los productos
+    // Pueden haber +2 productos con el mismo id pero diferente size
+
+    const products = await prisma.product.findMany({
+      where: {
+        id: {
+          in: productsInOrder.map(p => p.productId)
+        }
+      }
+    });
+
+    // Total items in order
+    const itemsInOrder = productsInOrder.reduce((count, p) => count + p.quantity, 0)
+
+
+    // Variables para calculo de montos
+    let subTotal = 0;
+    let tax = 0;
+    let total = 0;
+
+    // Cálculo de montos
+    for (const item of productsInOrder) {
+      const product = products.find(p => p.id === item.productId);
+
+      if (!product) {
+        throw new Error(`${item.productId} not exist - 500`);
+      }
+
+      const itemSubTotal = product.price * item.quantity;
+      subTotal += itemSubTotal;
+      tax += itemSubTotal * 0.15;
+      total += itemSubTotal * 1.15;
+    }
+
+    console.log({ subTotal, tax, total });
+
+
+
     return 'Todo ok'
 
 
