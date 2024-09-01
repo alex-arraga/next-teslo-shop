10bc7ccd06910f44f4cfac42e515abb8fb1b09f2
alex-arraga <alex-arraga@users.noreply.github.com>
2024-09-01 19:48:44 -0300

    Skeleton btn

--- src/components/paypal/PayPalButton.tsx
+++ src/components/paypal/PayPalButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { PayPalButtons } from "@paypal/react-paypal-js"
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js"
 
 
 interface Props {
@@ -9,6 +9,18 @@ interface Props {
 
 
 export const PayPalButton = ({ maxWith = true }: Props) => {
+  const [{ isPending }] = usePayPalScriptReducer();
+
+  if (isPending) {
+    return (
+      <div className="flex flex-col items-center gap-4 animate-pulse transition-all">
+        <div className="bg-gray-200 w-full h-12 rounded" />
+        <div className="bg-gray-200 w-full h-12 rounded" />
+        <div className="bg-gray-200 w-32 h-4 rounded" />
+      </div>
+    )
+  }
+
   return (
     <PayPalButtons
       style={{
