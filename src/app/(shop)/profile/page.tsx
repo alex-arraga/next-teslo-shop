54a686c9d912ae530a5e437da617dc4eb36c7bae
alex-arraga <alex-arraga@users.noreply.github.com>
2025-04-08 10:06:49 -0300

    improving mobile sizes of profile

--- src/app/(shop)/profile/page.tsx
+++ src/app/(shop)/profile/page.tsx
@@ -14,9 +14,9 @@ export default async function ProfilePage() {
     <section className="min-h-screen">
       <Title title="Perfil" />
 
-      <div className="bg-white dark:bg-neutral-900 p-4 xl:p-6 rounded-lg max-w-xl">
+      <div className="bg-white dark:bg-neutral-900 p-2 sm:p-4 xl:p-6 rounded-lg max-w-xl">
         <h2 className="mb-4 font-medium text-sm md:text-lg">User data</h2>
-        <pre className="text-blue-900 dark:text-blue-100">
+        <pre className="text-blue-900 dark:text-blue-100 text-xs sm:text-sm md:text-base">
           {JSON.stringify(session, null, 2)}
         </pre>
       </div>
