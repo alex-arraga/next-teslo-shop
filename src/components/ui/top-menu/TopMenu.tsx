f8ca0e9953f563e0548d05ab4dc902d60091d233
alex-arraga <alex-arraga@users.noreply.github.com>
2025-04-08 17:33:27 -0300

    top menu translated and improved

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -12,6 +12,20 @@ import clsx from "clsx"
 import { IoCartOutline, IoSearchOutline } from "react-icons/io5"
 import { Gender } from "@prisma/client"
 
+const topMenuOptions = [
+  {
+    gender: "men",
+    href: "/gender/men",
+  },
+  {
+    gender: "women",
+    href: "/gender/women",
+  },
+  {
+    gender: "kids",
+    href: "/gender/kid",
+  },
+]
 
 export const TopMenu = () => {
   const [loaded, setLoaded] = useState<boolean>(false)
@@ -36,45 +50,25 @@ export const TopMenu = () => {
       </div>
 
       {/* Center menu */}
-      <div className="hidden sm:block">
-        <Link
-          href="/gender/men"
-          className={
-            clsx("m-2 p-2 rounded-md transition-all hover:bg-gray-100 dark:hover:bg-blue-100 dark:hover:text-gray-950",
-              {
-                "bg-blue-500 text-white dark:hover:text-white hover:bg-blue-600 dark:hover:bg-blue-600": params.gender === 'men'
+      <div className="hidden sm:flex">
+        {
+          topMenuOptions.map(option => (
+            <Link
+              href={option.href}
+              className={
+                clsx("m-2 py-2 px-4 rounded-md transition-all text-center hover:bg-gray-100 dark:hover:bg-blue-100 dark:hover:text-gray-950",
+                  {
+                    "bg-blue-500 text-white dark:hover:text-white hover:bg-blue-600 dark:hover:bg-blue-600": params.gender === 'men'
+                  }
+                )
               }
-            )
-          }
-        >
-          Hombres
-        </Link>
-
-        <Link
-          href="/gender/women"
-          className={
-            clsx("m-2 p-2 rounded-md transition-all hover:bg-gray-100 dark:hover:bg-blue-100 dark:hover:text-gray-950",
-              {
-                "bg-blue-500 text-white dark:hover:text-white hover:bg-blue-600 dark:hover:bg-blue-600": params.gender === 'women'
-              }
-            )
-          }
-        >
-          Mujeres
-        </Link>
-
-        <Link
-          href="/gender/kid"
-          className={
-            clsx("m-2 p-2 rounded-md transition-all hover:bg-gray-100 dark:hover:bg-blue-100 dark:hover:text-gray-950",
-              {
-                "bg-blue-500 text-white dark:hover:text-white hover:bg-blue-600 dark:hover:bg-blue-600": params.gender === 'kid'
-              }
-            )
-          }
-        >
-          Niños
-        </Link>
+            >
+              <p className="capitalize">
+                {option.gender}
+              </p>
+            </Link>
+          ))
+        }
       </div>
 
       {/* Right options: Menu, cart */}
