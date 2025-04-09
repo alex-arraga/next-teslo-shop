f15a169f0aae10d5fa175fb514464cf07e956719
alex-arraga <alex-arraga@users.noreply.github.com>
2025-04-09 12:09:57 -0300

    dark admin: users table

--- src/app/(shop)/admin/users/ui/UserTable.tsx
+++ src/app/(shop)/admin/users/ui/UserTable.tsx
@@ -13,25 +13,25 @@ export const UserTable = ({ users }: Props) => {
 
     <table className="min-w-full">
 
-      <thead className="bg-gray-200 border-b">
+      <thead className="bg-gray-200 border-b dark:border-neutral-600">
         <tr className=''>
-          <th scope="col" className="text-sm text-center w-fit bg-slate-100 font-semibold text-gray-900 px-6 py-4">
+          <th scope="col" className="text-sm text-center w-fit bg-slate-100 dark:bg-neutral-900 dark:text-gray-200 font-semibold text-gray-900 px-6 py-4">
             #ID
           </th>
-          <th scope="col" className="text-sm text-center w-fit bg-slate-100 font-semibold text-gray-900 px-6 py-4">
+          <th scope="col" className="text-sm text-center w-fit bg-slate-100 dark:bg-neutral-900 dark:text-gray-200 font-semibold text-gray-900 px-6 py-4">
             Email
           </th>
-          <th scope="col" className="text-sm text-center w-fit bg-slate-100 font-semibold text-gray-900 px-6 py-4">
-            Nombre
+          <th scope="col" className="text-sm text-center w-fit bg-slate-100 dark:bg-neutral-900 dark:text-gray-200 font-semibold text-gray-900 px-6 py-4">
+            Fullname
           </th>
-          <th scope="col" className="text-sm text-center w-fit bg-slate-100 font-semibold text-gray-900 px-6 py-4">
-            Pais
+          <th scope="col" className="text-sm text-center w-fit bg-slate-100 dark:bg-neutral-900 dark:text-gray-200 font-semibold text-gray-900 px-6 py-4">
+            Country
           </th>
-          <th scope="col" className="text-sm text-center w-fit bg-slate-100 font-semibold text-gray-900 px-6 py-4">
-            Telefono
+          <th scope="col" className="text-sm text-center w-fit bg-slate-100 dark:bg-neutral-900 dark:text-gray-200 font-semibold text-gray-900 px-6 py-4">
+            Phone
           </th>
-          <th scope="col" className="text-sm text-center w-fit bg-slate-100 font-semibold text-gray-900 px-6 py-4">
-            Rol
+          <th scope="col" className="text-sm text-center w-fit bg-slate-100 dark:bg-neutral-900 dark:text-gray-200 font-semibold text-gray-900 px-6 py-4">
+            Role
           </th>
         </tr>
       </thead>
@@ -40,31 +40,31 @@ export const UserTable = ({ users }: Props) => {
         {users?.map((user) => (
           <tr
             key={user.id}
-            className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+            className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100 dark:bg-zinc-700 dark:hover:bg-zinc-800 dark:border-neutral-800">
 
-            <td className="text-sm text-center text-gray-900  font-semibold px-6 py-4 whitespace-nowrap">
+            <td className="text-sm text-center text-gray-900 dark:text-gray-100 font-medium px-6 py-4 whitespace-nowrap">
               #{user.id.split('-').at(0)?.slice(0, -3)}
             </td>
 
-            <td className="text-sm text-center text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+            <td className="text-sm text-center text-gray-900 dark:text-gray-100 font-medium px-6 py-4 whitespace-nowrap">
               {user.email}
             </td>
 
-            <td className="text-sm text-center text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+            <td className="text-sm text-center text-gray-900 dark:text-gray-100 font-medium px-6 py-4 whitespace-nowrap">
               {user.name}
             </td>
 
-            <td className="text-sm text-center text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+            <td className="text-sm text-center text-gray-900 dark:text-gray-100 font-medium px-6 py-4 whitespace-nowrap">
               {user.address?.country.name ?? '-'}
             </td>
 
-            <td className="flex items-center justify-center text-sm text-center text-gray-900 font-medium px-6 py-4 whitespace-nowrap">
+            <td className="flex items-center justify-center text-sm text-center text-gray-900 dark:text-gray-100 font-medium px-6 py-4 whitespace-nowrap">
               {user.address?.phone}
             </td>
 
             <td className="text-sm text-center text-gray-900 font-medium px-6 ">
               <select
-                className="w-full p-1 bg-gray-100 rounded"
+                className="w-full p-1 bg-gray-100 text-gray-900 dark:bg-neutral-800 dark:text-white rounded"
                 value={user.role}
                 onChange={e => changeUserRole(user.id, e.target.value)}
               >
